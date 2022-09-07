623caba1da54ed370544d4969f570770f557107b
PedroSchlott <pedrohenriqueschlottfeldt@gmail.com>
2022-09-06 21:46:19 -0300

    final da aula

---
+++ exibir.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>.:Página com a criação da tabela:.</title>
+
+        <meta charset='utf-8'>
+    </head>
+<table border='1'>
+
+<tr><td>Tabela criada pelo FOR</td></tr>
+
+
+    <?php
+$quantidade = $_POST['quantidade'];
+echo $quantidade;
+
+    for($i = 1;$i <= $quantidade; $i++)
+
+        {
+
+        echo "<tr><td>$i</td></tr>";
+        
+        }
+    ?>
+
+</table>
+
+</html>
